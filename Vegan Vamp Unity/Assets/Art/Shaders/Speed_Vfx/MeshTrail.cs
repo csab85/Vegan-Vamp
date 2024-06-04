5d915d54e6518619d0334e95f1c5abe016ce42b6
csab85 <csantanna998@gmail.com>
2024-06-04 17:47:46 -0300

    speed shader fix

--- Vegan Vamp Unity/Assets/Art/Shaders/Speed_Vfx/MeshTrail.cs
+++ Vegan Vamp Unity/Assets/Art/Shaders/Speed_Vfx/MeshTrail.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MeshTrail : MonoBehaviour
 {
     public float activeTime = 2f;
 
