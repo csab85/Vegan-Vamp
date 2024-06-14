5465d0234d83a054fab494fdc929ee807598ae84
csab85 <csantanna998@gmail.com>
2024-06-13 21:47:41 -0300

    teleport plant puzzle
    made it teleport around (gotta still add fx)

--- Vegan Vamp Unity/Assets/Programming/Scripts/Ingredients/TeleportShroom.cs
+++ Vegan Vamp Unity/Assets/Programming/Scripts/Ingredients/TeleportShroom.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class TeleportShroom : MonoBehaviour
 {
@@ -6,7 +8,7 @@ public class TeleportShroom : MonoBehaviour
     //========================
     #region
 
-
+    [SerializeField] VisualEffect particles;
 
     #endregion
     //========================
@@ -16,7 +18,9 @@ public class TeleportShroom : MonoBehaviour
     //========================
     #region
 
-
+    [SerializeField] public float areaRadius;
+    [SerializeField] float tpCooldown;
+    [HideInInspector] public Vector3 areaCenter;
 
     #endregion
     //========================
@@ -26,7 +30,21 @@ public class TeleportShroom : MonoBehaviour
     //========================
     #region
 
+    IEnumerator RandomTeleport()
+    {
+        float angle = Random.Range(0, Mathf.PI * 2);
+        float distance = Random.Range(0, areaRadius);
 
+        float circleX = areaCenter.x + Mathf.Cos(angle) * distance;
+        float circleZ = areaCenter.z + Mathf.Sin(angle) * distance;
+
+        transform.position = new Vector3(circleX, 0, circleZ);
+        particles.Play();
+
+        yield return new WaitForSeconds(tpCooldown);
+
+        StartCoroutine(RandomTeleport());
+    }
 
     #endregion
     //========================
@@ -36,7 +54,11 @@ public class TeleportShroom : MonoBehaviour
     //========================
     #region
 
-
+    void Start()
+    {
+        areaCenter = transform.position;
+        StartCoroutine(RandomTeleport());
+    }
 
     #endregion
     //========================
