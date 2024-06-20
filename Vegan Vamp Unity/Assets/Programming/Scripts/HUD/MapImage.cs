1033a87836071f039cea6ab9f35c0c9c687445a1
csab85 <csantanna998@gmail.com>
2024-06-20 19:58:06 -0300

    ui changes

---
+++ Vegan Vamp Unity/Assets/Programming/Scripts/HUD/MapImage.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class MapImage: MonoBehaviour
+{
+    //IMPORTS
+    //========================
+    #region
+
+    [SerializeField] Transform player;
+
+    #endregion
+    //========================
+
+
+    //STATS AND VALUES
+    //========================
+    #region
+
+
+
+    #endregion
+    //========================
+
+
+    //FUNCTIONS
+    //========================
+    #region
+
+
+
+    #endregion
+    //========================
+
+
+    //RUNNING
+    //========================
+    #region
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+
+    #endregion
+    //========================
+
+
+}
