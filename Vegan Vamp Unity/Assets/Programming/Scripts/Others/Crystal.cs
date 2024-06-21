94da4563c0b4e28a80e280963f0d75eefe9af6d2
csab85 <csantanna998@gmail.com>
2024-06-21 01:49:25 -0300

    unfinished crystal

---
+++ Vegan Vamp Unity/Assets/Programming/Scripts/Others/Crystal.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    //IMPORTS
+    //========================
+    #region
+
+    [SerializeField] GameObject intact;
+    [SerializeField] GameObject broke;
+
+    //scripts
+    StatsManager selfStats;
+
+    #endregion
+    //========================
+
+
+    //STATS AND VALUES
+    //========================
+    #region
+
+
+
+    #endregion
+    //========================
+
+
+    //FUNCTIONS
+    //========================
+    #region
+
+
+
+    #endregion
+    //========================
+
+
+    //RUNNING
+    //========================
+    #region
+
+
+
+    #endregion
+    //========================
+
+
+}
