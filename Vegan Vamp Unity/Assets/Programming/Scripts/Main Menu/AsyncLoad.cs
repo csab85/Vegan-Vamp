8cbbb8a0c06c14deefb1bd6b3056f6f6a143a430
csab <csantanna998@gmail.com>
2024-09-13 15:54:12 -0300

    new load

--- Vegan Vamp Unity/Assets/Programming/Scripts/Main Menu/AsyncLoad.cs
+++ Vegan Vamp Unity/Assets/Programming/Scripts/Main Menu/AsyncLoad.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using TMPro;
 
 public class AsyncLoad : MonoBehaviour
 {
@@ -13,8 +14,8 @@ public class AsyncLoad : MonoBehaviour
     [SerializeField] GameObject loadingScreen;
     [SerializeField] GameObject mainMenu;
 
-    //scripts
-    [SerializeField] LoadText loadText;
+    //component
+    [SerializeField] TextMeshProUGUI loadText;
 
     AsyncOperation loadOperation;
 
@@ -36,13 +37,16 @@ public class AsyncLoad : MonoBehaviour
     //========================
     #region
 
-    public void LoadLevel(string levelToLoad)
+    public void StartLoad()
     {
+        //deactivate menu and activate loading screen
         mainMenu.SetActive(false);
         loadingScreen.SetActive(true);
+
         loading = true;
-        
-        StartCoroutine(LoadLevelAsync(levelToLoad));
+
+        //start async load
+        StartCoroutine(LoadLevelAsync("NewOpenWorld"));
     }
 
     IEnumerator LoadLevelAsync(string levelToLoad)
@@ -66,112 +70,17 @@ public class AsyncLoad : MonoBehaviour
     {
         if (loading)
         {
-            switch (loadText.deletedTexts)
+            //update load text
+            loadText.text = loadOperation.progress < 0.9f ? $"Carregando - {loadOperation.progress * 100}%" : "Carregando - 100%\nAperte ESPAÇO para começar";
+
+            //Load if scene loaded and space pressed
+            if (Input.GetKeyDown(KeyCode.Space) && loadOperation.progress >= 0.9f)
             {
-                case 0:
-
-                    if (loadText.printedTexts == 0)
-                    {
-                        //wait a bit
-                        if (!loadText.waiting && !loadText.waited)
-                        {
-                            StartCoroutine(loadText.PrintText(0, 1, true));
-                        }
-
-                        //print first text
-                        if (!loadText.typing && !loadText.waiting)
-                        {
-                            StartCoroutine(loadText.PrintText(1, 0.1f));
-                        }
-                    }
-
-                    if (loadText.printedTexts == 1)
-                    {
-                        //wait a bit
-                        if (!loadText.waiting && !loadText.waited)
-                        {
-                            StartCoroutine(loadText.PrintText(0, 1, true));
-                        }
-
-                        //print second text
-                        if (!loadText.typing && !loadText.waiting)
-                        {
-                            StartCoroutine(loadText.PrintText(2, 0.1f));
-                        }
-                    }
-
-                    if (loadText.printedTexts == 2)
-                    {
-                        //wait a bit
-                        if (!loadText.waiting && !loadText.waited)
-                        {
-                            StartCoroutine(loadText.PrintText(0, 1, true));
-                        }
-
-                        //print third text
-                        if (!loadText.typing && !loadText.waiting)
-                        {
-                            StartCoroutine(loadText.PrintText(3, 0.1f));
-                        }
-                    }
-
-                    //delete third text
-                    if (loadText.printedTexts == 3)
-                    {
-                        //give some time for player to read
-                        if (!loadText.waiting && !loadText.waited)
-                        {
-                            StartCoroutine(loadText.PrintText(0, 3, true));
-                        }
-
-                        if (!loadText.typing && !loadText.waiting)
-                        {
-                            StartCoroutine(loadText.DeleteText(3, 0.03f));
-                        }
-                    }
-
-                    break;
-
-                case 1:
-
-                    //delete second text
-                    if (!loadText.typing)
-                    {
-                        StartCoroutine(loadText.DeleteText(2, 0.03f));
-                    }
-
-                    break;
-
-                case 2:
-
-                    //delete first text
-                    if (!loadText.typing)
-                    {
-                        StartCoroutine(loadText.DeleteText(1, 0.03f));
-                    }
-
-                    break;
-
-                case 3:
-
-                    //wait then delete scene
-                    if (!loadText.waiting && !loadText.waited)
-                    {
-                        StartCoroutine(loadText.PrintText(0, 1, true));
-                    }
-
-                    if (!loadText.waiting && loadText.waited)
-                    {
-                        loadOperation.allowSceneActivation = true;
-                    }
-
-                    break;
+                loadOperation.allowSceneActivation = true;
             }
         }
     }
 
     #endregion
     //========================
-
-
 }
